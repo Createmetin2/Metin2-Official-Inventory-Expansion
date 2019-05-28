c67767534a9af134577f95abb63e0b54ec37f7a7
Muhammet Ali **** <33325253+blackdragonx61@users.noreply.github.com>
2019-05-29 00:39:32 +0300

    Code Improve

--- 1.Svn/Server/game/src/c++11(+)/exchange.cpp
+++ 1.Svn/Server/game/src/c++11(+)/exchange.cpp
@@ -16,10 +16,10 @@ bool CExchange::CheckSpace()
 	
 	for (size_t i = 0; i < s_grid.size(); i++)
     {
-        if (i < default_open_inven) {
-            s_grid[i] = new CGrid(5,9);
-            continue;
-        }
+		if (i < default_open_inven) {
+			s_grid[i] = new CGrid(5,9);
+			continue;
+		}
         if (invenpoint > 0 && invenpoint <= 9)
             new_size = invenpoint;
         else if (invenpoint >= 9)
@@ -28,18 +28,18 @@ bool CExchange::CheckSpace()
             new_size = 0;
         s_grid[i] = new CGrid(5,new_size);
         invenpoint-=9;
-    }
-   for (size_t j = 0; j < s_grid.size(); j++) {
-        s_grid[j]->Clear();
-        for (auto i = INVEN_NUM_SLOT * j; i < INVEN_NUM_SLOT * (j+1); ++i) {
+	}
+	for (size_t j = 0; j < s_grid.size(); j++) {
+		s_grid[j]->Clear();
+		for (auto i = INVEN_NUM_SLOT * j; i < INVEN_NUM_SLOT * (j+1); ++i) {
     		if (!(item = GetCompany()->GetOwner()->GetInventoryItem(i))) 
     		    continue;
     		s_grid[j]->Put(i - INVEN_NUM_SLOT * j, 1, item->GetSize());
 		}
 	}
-    for (auto i = 0; i < EXCHANGE_ITEM_MAX_NUM; ++i) {
-        if (!(item = m_apItems[i])) continue;
-        if (item->IsDragonSoul()) {
+	for (auto i = 0; i < EXCHANGE_ITEM_MAX_NUM; ++i) {
+		if (!(item = m_apItems[i])) continue;
+		if (item->IsDragonSoul()) {
             if (!GetCompany()->GetOwner()->DragonSoul_IsQualified()) return false;
             if (!bDSInitialized) {
                 bDSInitialized = true;
